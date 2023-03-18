2f8a3995490c42fe5e66e8abafdf04246c6f6c26
Bernardo Erosa <bernardo.emex@prepafrayangelico.edu.mx>
2023-03-17 18:59:42 -0600

    juegoo

--- juego.js
+++ juego.js
@@ -1,6 +1,14 @@
 let nave=document.querySelector(".nave");
 let body=document.querySelector("body");
-document.addEventListener("mousemove",()=>{
+document.addEventListener("mousemove",(e)=>{
     nave.stylejuego.left=e.clientX+"px";
 })
+//generar disparo
+document.addEventListener("click" ,()=>{
+    let bala=document.createElement("div");
+    bala.classList.add("bala");
+    bala.stylejuego.bottom=70+"px";
+    bala.stylejuego.left=(nave.getBoundingClientRect().left+40)
+    body.append(bala);
 
+})
