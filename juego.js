5f5762c768afa52ab22799ddfbebb47c5c23504d
Bernardo Erosa <bernardo.emex@prepafrayangelico.edu.mx>
2023-05-08 12:24:33 -0600

    avancee
    .

--- juego.js
+++ juego.js
@@ -1,6 +1,14 @@
 let nave=document.querySelector('.nave');
 let body=document.querySelector('body');
 let laser=document.getElementById('laser');
+let live=document.querySelector("i");
+let times= document.getElementById("times");
+let lives=5;
+let second=60;
+setInterval(() => {
+    second--;
+    times.textContent=second;
+},1000)
 document.addEventListener('mousemove',(e)=> {
     nave.style.left=(e.clientX-40)+'px';
 });
@@ -25,5 +33,29 @@ setInterval(()=>{;
 
     });
 },100);  
-   
+
+//generar meteoritos
+let aparecer=0;
+setInterval(()=>{
+    aparecer++;
+    if(aparecer%10==0){
+    let enemigo=document.createElement("div");
+    enemigo.classList.add("enemigo");
+    body.append(enemigo);
+    enemigo.style.left=(Math.random()*window.innerWidth-100)
+    +"px";
+    }
+    let enemigos=document.querySelectorAll(".enemigo");
+    enemigos.forEach(element=> {
+        element.style.top=(element.getBoundingClientRect().top
+        +10)+"px";
+        if (element.getBoundingClientRect().top>nave.
+        getBoundingClientRect().top){
+            lives--;
+            live.textContent=lives;
+            element.remove();
+        }
+    });
+},100);
+
     
